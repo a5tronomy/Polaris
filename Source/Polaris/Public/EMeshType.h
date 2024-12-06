a8667ab025c65cc9e61fbb1440ff18205336daea
a5tronomy <67793178+a5tronomy@users.noreply.github.com>
2024-12-06 22:03:31 +0100

    Update Enumerations

--- Source/Polaris/Public/EMeshType.h
+++ Source/Polaris/Public/EMeshType.h
@@ -4,9 +4,9 @@
 
 UENUM()
 enum class EMeshType : uint8 {
-    E_SkeletalMesh,
-    E_SkeletalMeshArray,
-    E_StaticMesh,
-    E_NoneStaticMesh,
+    E_SkeletalMesh UMETA(DisplayName = "Skeletal Mesh"),
+    E_SkeletalMeshArray UMETA(DisplayName = "Skeletal Mesh Array"),
+    E_StaticMesh UMETA(DisplayName = "Static Mesh"),
+    E_NoneStaticMesh UMETA(DisplayName = "None Static Mesh"),
 };
 
--- Source/Polaris/Public/EStageImpulseStrengthType.h
+++ Source/Polaris/Public/EStageImpulseStrengthType.h
@@ -5,10 +5,10 @@
 UENUM()
 enum class EStageImpulseStrengthType : uint8 {
     None,
-    W,
-    W_Mid,
-    S,
-    SS,
+    W UMETA(DisplayName = "Normal Hit"),
+    W_Mid UMETA(DisplayName = "Mid Normal Hit"),
+    S UMETA(DisplayName = "Strong Hit"),
+    SS UMETA(DisplayName = "Special Strong Hit"),
     Term,
 };
 
